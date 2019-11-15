9747a8962da6754de2aeef244202e294bf9f55fc
MA JIANING <1256329520@qq.com>
2019-11-15 10:08:03 +0800

    提交

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Router } from 'dva/router';
+
+// 引入封装的路由表
+import routes from './router.config';
+import RouterView from './RouterView';
+
+function RouterConfig({ history }) {
+  console.log('routes...',routes);
+  return (
+    <Router history={history}>
+      {/* 父传子 用props */}
+      <RouterView routes={routes.routes}/>
+      {console.log(1111)}
+      {/* <Switch>
+        <Route path="/" exact component={IndexPage} />
+        <Route path="/main/addGov" exact component={IndexPage} />
+        <Route path="/main/addPro" exact component={IndexPage} />
+
+        <Route path="/abc/def" exact component={IndexPage} />
+      </Switch> */}
+    </Router>
+  );
+}
+
+export default RouterConfig;
